a2e092c339fa30e41defee3bf5b6d107019ec9a9
huseyincaglar1 <huseyincaglar309@gmail.com>
2022-10-17 11:04:24 +0300

    .

---
+++ hafta_1/5_metinler.py
@@ -0,0 +1,14 @@
+a = "merhaba"
+b = """mer
+ha
+ba"""
+print(a)
+print(b)
+print(a[0])
+print(a[1])
+print(b[3]) # b değişkenindeki boşluklar da sayılıyor.
+print(b[6]) # bu değer boşluğa denk geldiği için çıktı boş görünecektir.
+
+print("a nın uzuluğu :", len(a))
+print("b nin uzuluğu :", len(b))
+
