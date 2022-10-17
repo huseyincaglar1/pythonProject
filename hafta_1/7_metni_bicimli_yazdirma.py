8af7e96b575dba4c880a4c02c5a243aaf4ae980b
huseyincaglar1 <huseyincaglar309@gmail.com>
2022-10-17 11:20:36 +0300

    .

---
+++ hafta_1/7_metni_bicimli_yazdirma.py
@@ -0,0 +1,9 @@
+a = 5
+b = 10
+c = 15
+print("a = {} / b = {} / c = {}".format(a, b, c))
+#format() metodu değerin istediğimiz biçimde yazdırılmasını sağlar.
+
+print("hUSEyin".capitalize())#karakter dizsinin ilk harfini büyük kalanını küçük harfe çevirir.
+print("HUseyin".upper())#karakter dizisinin tamamını büyük harfe çevirir.
+print("HUseyin".lower())#karakter dizisinin tamamını küçük harfe çevirir.
