6ca5bc58b655f7858112c73cc2dfd3f88b40315e
huseyincaglar1 <huseyincaglar309@gmail.com>
2022-10-17 11:11:48 +0300

    .

---
+++ hafta_1/6_metin_karsılastırma.py
@@ -0,0 +1,13 @@
+ad = "Huseyin"
+
+if ad == "Huseyin":
+    print("ad Huseyin'e eşit")
+
+if "Hus" in ad:
+    print("ad da Hus var")
+
+if "HUS" in ad: #büyük küçük harfin önemi var.
+    print("ad da HUS var")
+
+if "ey" in ad:
+    print("ad da ey var")
