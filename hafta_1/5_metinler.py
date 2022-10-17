7970ea3eec9b7cd7ca97886bc3008ef61d58daef
huseyincaglar1 <huseyincaglar309@gmail.com>
2022-10-17 11:08:24 +0300

    .

--- hafta_1/5_metinler.py
+++ hafta_1/5_metinler.py
@@ -9,6 +9,6 @@ print(a[1])
 print(b[3]) # b değişkenindeki boşluklar da sayılıyor.
 print(b[6]) # bu değer boşluğa denk geldiği için çıktı boş görünecektir.
 
-print("a nın uzuluğu :", len(a))
+print("a nın uzuluğu :", len(a))#len komutu metin uzunluğunu bulur.
 print("b nin uzuluğu :", len(b))
 
