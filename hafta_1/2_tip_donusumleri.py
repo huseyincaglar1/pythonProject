12e29ab13002fc02e1d70ad6f2b2a6afc27ce4f3
huseyincaglar1 <huseyincaglar309@gmail.com>
2022-10-17 03:34:58 +0300

    .

---
+++ hafta_1/2_tip_donusumleri.py
@@ -0,0 +1,14 @@
+sayi = 5
+print(type(sayi))
+sayi = float(5)
+print(type(sayi))
+sayi = str(5)
+print(type(sayi))
+class sinif:
+    sayi2 = 8.6
+
+t = sinif
+print(type(sinif))
+print(type(t))
+print(t.sayi2)
+print(type(t.sayi2))
