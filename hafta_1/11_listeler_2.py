e2052b0d7434a7934d3598096c8b18dee5266e38
huseyincaglar1 <huseyincaglar309@gmail.com>
2022-10-22 13:07:09 +0300

    .

--- hafta_1/11_listeler_2.py
+++ hafta_1/11_listeler_2.py
@@ -1,19 +1,24 @@
 a = [1, 6]
+a.append(5) # yeni elemanı listenin en sonuna ekler.
+print(a)
+
+a.pop() # indeks numarası belirtmezsek listenin son elemanını siler
+print(a)
+"""
+del() metodu da aynı görevi görür.
+fakat del metoduna indeks numarası 
+vermezsek liste silinir.
+"""
+
 a.append(5)
+a.remove(1) # listedeki silmek istediğimiz elemanı direk bu metoda yazmalıyız.
 print(a)
 
-a.pop()
+a.insert(0, 100) # listede istediğimiz indekse eleman eklememizi sağlar.
 print(a)
 
-a.append(5)
-a.remove(1)
+print(a.pop()) # listeden silinen elemanı yazdırır.
 print(a)
 
-a.insert(1, 100)
-print(a)
-
-print(a.pop())
-print(a)
-
-a.clear()
+a.clear() # bu metod listenin içeriğini siler.
 print(a)
